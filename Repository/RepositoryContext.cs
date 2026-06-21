92f182a4463bf41c6986e66490d10a939166274b
Thami <thamig125@gmail.com>
2026-06-21 19:28:04 +0200

    feature: add context class to Repository

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repository.Configuration;
+
+namespace Repository;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new AddressConfiguration());
+        modelBuilder.ApplyConfiguration(new ApartmentConfiguration());
+        modelBuilder.ApplyConfiguration(new ImageConfiguration());
+    }
+
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Apartment> Apartments { get; set; }
+    public DbSet<Image> Images { get; set; }
+}
