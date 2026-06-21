9b910f741207e06dc38d6420da04db96c51f4656
Thami <thamig125@gmail.com>
2026-06-21 19:12:46 +0200

    feature: add DesignTime context for migrations

---
+++ RoomRental.Server/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace RoomRental.Server.ContextFactory;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+{
+    public RepositoryContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+         .SetBasePath(Directory.GetCurrentDirectory())
+         .AddJsonFile("appsettings.json")
+         .Build();
+
+        var builder = new DbContextOptionsBuilder<RepositoryContext>()
+                   .UseNpgsql(configuration.GetConnectionString("postgresConnection"),
+                   b => b.MigrationsAssembly("RoomRental.Server"));
+
+        return new RepositoryContext(builder.Options);
+    }
+}
