4bed20cc19f22e6d5b92310af4cca3a37a2ba68f
Thami <thamig125@gmail.com>
2026-06-22 23:59:32 +0200

    fix: add price field to Apartment table

---
+++ RoomRental.Server/Migrations/20260621235508_AddPriceField.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoomRental.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Apartments",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Apartments",
+                keyColumn: "ApartmentId",
+                keyValue: new Guid("7c9e6679-7425-40de-944b-e07fc1f90ae7"),
+                column: "Price",
+                value: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Apartments");
+        }
+    }
+}
