6fed705d1ac63db6b584ea042563de8c92dd212b
Thami <thamig125@gmail.com>
2026-06-22 23:43:18 +0200

    feature: create AddressRepository

---
+++ Repository/AddressRepository.cs
@@ -0,0 +1,10 @@
+﻿using Contracts;
+using Entities.Models;
+
+namespace Repository;
+
+public class AddressRepository : RepositoryBase<Address>, IAddressRepository
+{
+    public AddressRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+    { }
+}
