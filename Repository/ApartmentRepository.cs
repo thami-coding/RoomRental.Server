774a0e2359f31864e964b7e886d82e111843a796
Thami <thamig125@gmail.com>
2026-06-22 23:44:34 +0200

    feature: create ApartmentRepository to inherit from RepositoryBase and IAprtmentRepository

---
+++ Repository/ApartmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Contracts;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository;
+
+public class ApartmentRepository : RepositoryBase<Apartment>, IApartmentRepository
+{
+    public ApartmentRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+    { }
+
+    public IEnumerable<Apartment> GetAllApartments(bool trackChanges) =>
+        FindAll(trackChanges).Include(a => a.Address).OrderBy(apartment => apartment.AvailableRooms).ToList();
+}
