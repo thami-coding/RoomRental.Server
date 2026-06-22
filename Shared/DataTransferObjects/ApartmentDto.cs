ea37396279165c63fca3887cdd81496153887433
Thami <thamig125@gmail.com>
2026-06-22 23:58:15 +0200

    feature: add ApartmentDto class

---
+++ Shared/DataTransferObjects/ApartmentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.DataTransferObjects;
+
+public class ApartmentDto
+{
+    public Guid Id { get; set; }
+    public string? Title { get; set; }
+    public int AvailableRooms { get; set; }
+    public string? Description { get; set; }
+    public AddressDto? Address { get; set; }
+}
