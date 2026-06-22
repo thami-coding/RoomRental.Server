57042508383ff2f2d166a4e60a4270a923f9fb65
Thami <thamig125@gmail.com>
2026-06-22 23:46:43 +0200

    feature: add RepositoryManager to implement IRepositoryManager interface

---
+++ Repository/RepositoryManager.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+
+namespace Repository;
+
+public sealed class RepositoryManager : IRepositoryManager
+{
+    private readonly RepositoryContext _repositoryContext;
+    private readonly Lazy<IApartmentRepository> _apartmentRepository;
+    private readonly Lazy<IAddressRepository> _addressRepository;
+
+    public RepositoryManager(RepositoryContext repositoryContext)
+    {
+        _repositoryContext = repositoryContext;
+        _addressRepository = new Lazy<IAddressRepository>(() => new AddressRepository(repositoryContext));
+        _apartmentRepository = new Lazy<IApartmentRepository>(() => new ApartmentRepository(repositoryContext));
+    }
+
+    public IApartmentRepository Apartment => _apartmentRepository.Value;
+    public IAddressRepository Address => _addressRepository.Value;
+
+    public void Save() => _repositoryContext.SaveChanges();
+}
