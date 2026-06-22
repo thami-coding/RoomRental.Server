fa6d85092de16b66e0a457b7e4371a55b249fcc2
Thami <thamig125@gmail.com>
2026-06-22 23:57:27 +0200

    feature: add AddressDto class

---
+++ Shared/DataTransferObjects/AddressDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.DataTransferObjects;
+
+public class AddressDto
+{
+    public Guid Id { get; set; }
+    public string? Street { get; set; }
+    public string? Province { get; set; }
+    public string? City { get; set; }
+    public string? Suburb { get; set; }
+}
