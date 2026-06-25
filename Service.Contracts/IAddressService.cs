c5ee67117601e1644b09f019671311c2409efa04
Thami <thamig125@gmail.com>
2026-06-25 20:16:56 +0200

    feature: add GetAddress method  for IAddressService interface

--- Service.Contracts/IAddressService.cs
+++ Service.Contracts/IAddressService.cs
@@ -1,5 +1,9 @@
-﻿namespace Service.Contracts;
+﻿using Shared.DataTransferObjects;
+using System.Net;
+
+namespace Service.Contracts;
 
 public interface IAddressService
 {
+    AddressDto GetAddress(Guid addressId, Guid id, bool trackChanges);
 }
