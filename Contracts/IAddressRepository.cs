4e88ecc94f3707d7462f87f42b2bfff7dd0f4105
Thami <thamig125@gmail.com>
2026-06-25 03:24:22 +0200

    feature: add GetAddress interface to IAddressRepository

--- Contracts/IAddressRepository.cs
+++ Contracts/IAddressRepository.cs
@@ -1,5 +1,8 @@
-﻿namespace Contracts;
+﻿using Entities.Models;
+
+namespace Contracts;
 
 public interface IAddressRepository
 {
+    IEnumerable<Address> GetAddress(Guid addressId, Guid id, bool trackChanges);
 }
