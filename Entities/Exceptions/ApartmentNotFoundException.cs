6e3ce973baaaa547976e427cf113ff216a093abb
Thami <thamig125@gmail.com>
2026-06-25 03:20:22 +0200

    feature: add ApartmentNotFound class to handle Not found apartments exception

---
+++ Entities/Exceptions/ApartmentNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities.Exceptions;
+
+public sealed class ApartmentNotFoundException : NotFoundException
+{
+    public ApartmentNotFoundException(Guid apartmentId) : base($"The apartment with id: {apartmentId} doesn't exist in the database")
+    { }
+}
