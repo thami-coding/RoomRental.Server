3c2f2baa4934f7cf7f194d622a05ba5b7704d1a3
Thami <thamig125@gmail.com>
2026-06-25 19:41:24 +0200

    feature: add GetAddress implementation for IAddressRepository to retrieve address

--- Repository/AddressRepository.cs
+++ Repository/AddressRepository.cs
@@ -5,6 +5,13 @@ namespace Repository;
 
 public class AddressRepository : RepositoryBase<Address>, IAddressRepository
 {
-    public AddressRepository(RepositoryContext repositoryContext) : base(repositoryContext)
-    { }
+    public AddressRepository(RepositoryContext repositoryContext)
+        : base(repositoryContext) { }
+
+
+    public Address GetAddress(Guid apartmentId, Guid id, bool trackChanges) =>
+        FindByCondition(a => a.ApartmentId.Equals(apartmentId) && a.Id.Equals(id), trackChanges)
+        .SingleOrDefault();
+
+
 }
