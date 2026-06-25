e171a10c0aa1aa50269d7592c67f240e2e85eb9b
Thami <thamig125@gmail.com>
2026-06-25 19:36:42 +0200

    feature: add exception class to handle Adress not found exceptions

---
+++ Entities/Exceptions/AddressNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities.Exceptions;
+
+public sealed class AddressNotFoundException : NotFoundException
+{
+    public AddressNotFoundException(Guid addressId)
+        : base($"Address with id: {addressId} deosn't exist in the database") { }
+}
