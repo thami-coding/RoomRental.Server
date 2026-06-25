105cc080bbccda7b8ef391080f3cd81dd8b17a49
Thami <thamig125@gmail.com>
2026-06-25 04:09:12 +0200

    feature: add address controller

---
+++ RoomRental.Presentation/Controllers/AddressesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace RoomRental.Presentation.Controllers;
+
+[Route("apartments/{apartmentId}/address")]
+[ApiController]
+public class AddressesController: ControllerBase
+{
+    private readonly IServiceManager _service;
+
+
+    public AddressesController(IServiceManager service) => _service = service;
+  
+}
