b625ba7faeec0dd7855c9560f668eff91dba899d
Thami <thamig125@gmail.com>
2026-06-25 19:38:16 +0200

    refactor: change return type to Address

--- Contracts/IAddressRepository.cs
+++ Contracts/IAddressRepository.cs
@@ -4,5 +4,5 @@ namespace Contracts;
 
 public interface IAddressRepository
 {
-    IEnumerable<Address> GetAddress(Guid addressId, Guid id, bool trackChanges);
+   Address GetAddress(Guid addressId, Guid id, bool trackChanges);
 }
