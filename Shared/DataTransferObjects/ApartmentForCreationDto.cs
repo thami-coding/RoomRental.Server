23e606784c6b7390e9a6021e7d846d3f3d35ac52
Thami <thamig125@gmail.com>
2026-06-25 22:31:50 +0200

    feature: create ApartmentDto for Apartment creation response

---
+++ Shared/DataTransferObjects/ApartmentForCreationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.DataTransferObjects;
+
+public class ApartmentForCreationDto
+{
+    public string? Title { get; set; }
+    public int AvailableRooms { get; set; }
+    public string? Description { get; set; }
+    public AddressDto? Address { get; set; }
+}
