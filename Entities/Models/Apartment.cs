a1163ce10c0265bbe4c585bb285456197050dac6
Thami <thamig125@gmail.com>
2026-06-25 16:46:38 +0200

    remove AddressId property

--- Entities/Models/Apartment.cs
+++ Entities/Models/Apartment.cs
@@ -11,9 +11,6 @@ public class Apartment
     public int AvailableRooms { get; set; }
     public string? Description { get; set; }
     public decimal Price { get; set; }
-
-    [ForeignKey(nameof(Address))]
-    public Guid AddressId { get; set; }
     public Address? Address { get; set; }
     public ICollection<Image> Images { get; set; } = new List<Image>();
 
