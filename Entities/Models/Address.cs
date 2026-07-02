2418a18d009520e38a27987dbf83a1b923e37480
Thami <thamig125@gmail.com>
2026-07-03 01:09:39 +0200

    feature: add validation for entities

--- Entities/Models/Address.cs
+++ Entities/Models/Address.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entities.Models;
 
@@ -6,10 +7,24 @@ public class Address
 {
     [Column("AddressId")]
     public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Street is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Street is 60 characters.")]
     public string? Street { get; set; }
+
+    [Required(ErrorMessage = "Province is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Province is 60 characters.")]
     public string? Province { get; set; }
+
+    [Required(ErrorMessage = "City is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the City is 60 characters.")]
     public string? City { get; set; }
+
+    [Required(ErrorMessage = "Suburb is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Suburb is 60 characters.")]
     public string? Suburb { get; set; }
+
+    [ForeignKey(nameof(Apartment))]
     public Guid ApartmentId { get; set; }
     public Apartment? Apartment { get; set; } 
 }
